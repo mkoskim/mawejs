e49982863e96c7b9f9ca22c04e38188a7b548867
Markus Koskimies <mkoskim@gmail.com>
2019-05-23 23:30:44 +0300

    Playing around.

--- src/plain.js
+++ src/plain.js
@@ -4,14 +4,42 @@ import Plain from 'slate-plain-serializer'
 import { Editor } from 'slate-react'
 import { Toolbar, Statusbar, Button, Icon, Separator } from "./toolbar"
 
-//-----------------------------------------------------------------------------
-
-const initialValue = Plain.deserialize(
-  'This is editable plain text, just like a <textarea>!'
-)
+import isHotkey from "is-hotkey"
 
 //-----------------------------------------------------------------------------
 
+const lorem =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+    "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+    "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
+    "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+    "culpa qui officia deserunt mollit anim id est laborum.\n\n"
+;
+
+//-----------------------------------------------------------------------------
+
+function AddLorem(hotkey) {
+    return {
+        onKeyDown(event, editor, next) {
+            if (isHotkey(hotkey, event)) {
+                editor.insertText(lorem)
+                console.log("insert")
+            } else {
+                return next()
+            }
+        },
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+const plugins = [
+    AddLorem("alt+l"),
+];
+
+//-----------------------------------------------------------------------------
 //*
 function PlainText(props)
 {
@@ -21,15 +49,16 @@ function PlainText(props)
                 <Icon name="format_bold"/>
                 <Icon name="format_italic"/>
                 <Separator/>
-                <Button>BTN</Button>
+                <Button>button</Button>
                 
                 <span style={{marginLeft: "auto"}} />
-                <Button>BTN</Button>
+                <Button>button</Button>
                 </Toolbar>
             <Editor
                 className = "editor"
                 placeholder="Enter some plain text..."
-                defaultValue={initialValue}
+                defaultValue={Plain.deserialize(lorem)}
+                plugins = {plugins}
             />
             <Statusbar>
                 <span className="word-count">XXX</span>
