c628e06a35078ef9e7f515261950e7a503a80a6d
Markus Koskimies <mkoskim@gmail.com>
2021-06-10 15:26:35 +0300

    App now sets initial flexbox
    Many components rely on fullsize flexbox at the top of them. Moved this to App component.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-import {FileBrowser, XFileBrowser} from "./gui/filebrowser/filebrowser";
+import {FileBrowser} from "./gui/filebrowser/filebrowser";
 
 export default function App(props) {
-  return (
-    //<FileBrowser />,
-    //<FileBrowser location="home"/>,
-    // <FileBrowser directory={undefined} />,
-    //<FileBrowser directory="./src" />,
-    //<FileBrowser directory="/dev" />,  
+  // Wrap views in a flex box filling the entire viewport.
+
+  return <FlexFull><View /></FlexFull>;
+
+  function View() {
+    return (
+    //<FileBrowser />
+    //<FileBrowser location="home"/>
+    // <FileBrowser directory={undefined} />
+    //<FileBrowser directory="./src" />
+    //<FileBrowser directory="/dev" />
     <FileBrowser directory="." />
-    //<FileBrowser directory="./node_modules" />,
+    //<FileBrowser directory="./node_modules" />
+    );
+  }
+}
+
+/*
+function FlexFull({children}) { return children; }
+/*/
+function FlexFull({style, children}) {
+  return (
+    <div style={{height: "100vh", width: "100vw", display: "flex", ...style}}>
+      {children}
+    </div>
   );
 }
+/**/
