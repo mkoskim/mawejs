de1b1f0e1a4d05e8e3af2dc24932b31a3c7d8de6
Markus Koskimies <mkoskim@gmail.com>
2022-06-20 14:08:26 +0300

    Updated app start

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import App from "./gui/app/app.js"
 import {store} from "./gui/app/store"
 import {Provider} from "react-redux"
 
-//import { DndProvider } from 'react-dnd'
-//import { HTML5Backend } from 'react-dnd-html5-backend'
-
+/*
+// React -18
+import ReactDOM from "react-dom";
 ReactDOM.render(
   <Provider store={store}>
       <App />
   </Provider>,
   document.getElementById("root")
 );
+/*/
+// React 18+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <Provider store={store}>
+      <App />
+  </Provider>
+);
+/**/
