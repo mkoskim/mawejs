2b59c9df35ff5fafe492bf28530f480085cf6581
Markus Koskimies <mkoskim@gmail.com>
2023-02-05 17:10:52 +0200

    Movable parts in organizer

--- src/gui/organizer/organizer.js
+++ src/gui/organizer/organizer.js
@@ -56,7 +56,8 @@ export function Organizer({id}) {
   function onDragEnd(result) {
     //console.log("onDragEnd:", result)
 
-    const {source, destination} = result;
+    const {type, source, destination} = result;
+    console.log(type)
 
     if(!destination) return;
 
@@ -67,18 +68,26 @@ export function Organizer({id}) {
       if(source.index === destination.index) return;
     }
 
-    const sourcePart = findPart(source.droppableId);
-    const destinationPart = findPart(destination.droppableId);
+    switch(type) {
+      case "scene": {
+        const sourcePart = findPart(source.droppableId);
+        const destinationPart = findPart(destination.droppableId);
 
-    const scene = sourcePart.children[source.index]
-    sourcePart.children.splice(source.index, 1)
-    destinationPart.children.splice(destination.index, 0, scene)
-    setDoc(doc)
-
-    //const sourcePart = findPart(source.droppableId)
-    //const destinationPart = findPart(destination.droppableId)
-
-    //console.log(sourcePart, "-->", destinationPart)
+        const scene = sourcePart.children[source.index]
+        sourcePart.children.splice(source.index, 1)
+        destinationPart.children.splice(destination.index, 0, scene)
+        setDoc(doc)
+        break;
+      }
+      case "part": {
+        const part = doc.story.body.parts[source.index]
+        doc.story.body.parts.splice(source.index, 1)
+        doc.story.body.parts.splice(destination.index, 0, part)
+        setDoc(doc)
+        break;
+      }
+      default: break;
+    }
   }
 }
 
@@ -86,19 +95,58 @@ function OrganizerView({doc}) {
   console.log("Organizer: Doc:", doc)
 
   return <div className="Filler" style={{overflow: "auto"}}>
-    <HBox style={{marginBottom: "1cm"}}>
-      {doc.story.body.parts.map(part => <PartView key={part.id} part={part}/>)}
-      </HBox>
+    <Droppable droppableId="body" direction="horizontal" type="part">
+      {partDroppable}
+    </Droppable>
     </div>
+
+  function partDroppable(provided, snapshot) {
+    const {innerRef, droppableProps, placeholder} = provided
+
+    return <div
+      ref={innerRef}
+      className="HBox Organizer" style={{marginBottom: "1cm"}}
+      {...droppableProps}
+      >
+      {doc.story.body.parts.map((part, index) => <PartView key={part.id} index={index} part={part}/>)}
+      {placeholder}
+      </div>
+  }
 }
 
-function PartView({part}) {
-  return <div className="Part">
-    <div className="Name">{part.name !== "" ? part.name : "???"}</div>
-    <Droppable droppableId={part.id}>{droppable}</Droppable>
-  </div>
+function PartView({part, index}) {
+  return <Draggable
+    draggableId={part.id}
+    index={index}
+    type="part"
+    >
+      {partDraggable}
+    </Draggable>
 
-  function droppable(provided, snapshot) {
+  function partDraggable(provided, snapshot) {
+    const {innerRef, draggableProps, dragHandleProps} = provided
+
+    return <div
+      ref={innerRef}
+      {...draggableProps}
+      className="Part"
+      >
+      <div
+        className="Name"
+        {...dragHandleProps}  // Move these inside to create handle
+      >
+        {part.name !== "" ? part.name : "???"}
+      </div>
+      <Droppable
+        droppableId={part.id}
+        type="scene"
+        >
+        {sceneDroppable}
+        </Droppable>
+    </div>
+  }
+
+  function sceneDroppable(provided, snapshot) {
     const {innerRef, droppableProps, placeholder} = provided
     const {
       isDraggingOver,   // true/false
@@ -116,7 +164,11 @@ function PartView({part}) {
 }
 
 function SceneView({scene, index}) {
-  return <Draggable draggableId={scene.id} index={index}>
+  return <Draggable
+    draggableId={scene.id}
+    index={index}
+    type="scene"
+    >
     {draggable}
   </Draggable>
 
