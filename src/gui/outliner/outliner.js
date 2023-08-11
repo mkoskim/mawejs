826838209c2042c1ddb71dc3e732c749628fcccd
Markus Koskimies <mkoskim@gmail.com>
2023-08-11 13:19:41 +0300

    Doc rendering
    Render the doc in to the view. Very basic at the moment.

--- src/gui/outliner/outliner.js
+++ src/gui/outliner/outliner.js
@@ -6,8 +6,55 @@
 //*****************************************************************************
 //*****************************************************************************
 
+import {elemAsText} from "../../document";
 import {Label} from "../common/factory";
 
 export function Outliner({doc, setDoc}) {
-  return <Label>Experimental</Label>
+  const {story} = doc
+  const {body} = story
+
+  return <div>
+    {body.parts.map(part => <PartItem key={part.id} part={part}/>)}
+  </div>
+
+}
+
+const styles = {
+  part: {
+    border: "1px dashed gray",
+    margin: "4pt",
+    padding: "4pt",
+  },
+  scene: {
+    border: "1px dotted grey",
+    marginTop: "4pt",
+    marginBottom: "4pt",
+    padding: "4pt",
+  },
+  parablock: {
+    border: "1px dotted grey",
+    marginTop: "4pt",
+    marginBottom: "4pt",
+    padding: "4pt",
+  }
+}
+
+function PartItem({part}) {
+  return <div style={styles.part}>
+    <Label>Part: {part.name}</Label>
+    {part.children.map(scene => <SceneItem key={scene.id} scene={scene}/>)}
+  </div>
+}
+
+function SceneItem({scene}) {
+  return <div style={styles.scene}>
+    <Label>Scene: {scene.name}</Label>
+    <ParaBlock paragraphs={scene.children}/>
+  </div>
+}
+
+function ParaBlock({paragraphs}) {
+  return <div style={styles.parablock}>
+    {paragraphs.map(p => <p key={p.id}>{elemAsText(p)}</p>)}
+  </div>
 }
