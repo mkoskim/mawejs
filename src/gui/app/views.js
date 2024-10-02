f2c5af5dbe8e3d469c6f08d9e3d50cc66d2eac7c
Markus Koskimies <mkoskim@gmail.com>
2024-10-02 23:39:59 +0300

    Organizer removed from views
    Organizer needs serious rework to be useful. Disabled for now.

--- src/gui/app/views.js
+++ src/gui/app/views.js
@@ -40,7 +40,7 @@ export class ViewSelectButtons extends React.PureComponent {
     />
   }
 
-  choices = ["editor", "organizer", "chart", "export"]
+  choices = ["editor", "chart", "export"]
 
   viewbuttons = {
     "editor": { tooltip: "Editor", icon: <Icon.View.Edit /> },
