f52015beca402755cac6953d02f1a6c109907a5a
Markus Koskimies <mkoskim@gmail.com>
2025-03-10 01:18:12 +0200

    Notes block markup reverted back to "%%"
    Even thought "/*" would be good, lets use "%%" at least now. We will design markups more carefully later.

--- src/document/elements.js
+++ src/document/elements.js
@@ -86,7 +86,7 @@ export const paragraphTypes = {
   "hchapter":  {name: "Chapter",  markup: "#",  shortcut: "Ctrl-Alt-2", eol: "p", bk: "p",},
   "hscene":    {name: "Scene",    markup: "##", shortcut: "Ctrl-Alt-3", eol: "p", bk: "p",},
   "hsynopsis": {name: "Synopsis", markup: ">>", shortcut: "Ctrl-Alt-S", eol: "p", bk: "p",},
-  "hnotes":    {name: "Notes",    markup: "/*", shortcut: "Ctrl-Alt-N", eol: "p", bk: "p",},
+  "hnotes":    {name: "Notes",    markup: "%%", shortcut: "Ctrl-Alt-N", eol: "p", bk: "p",},
 
   "comment":   {name: "Comment",  markup: "//", shortcut: "Ctrl-Alt-C",           bk: "p", reset: "p" },
   "missing":   {name: "Missing",  markup: "!!", shortcut: "Ctrl-Alt-M",           bk: "p", reset: "p" },
@@ -124,7 +124,7 @@ export const MARKUP = {
   "# " : {type: "hchapter"},
   "## ": {type: "hscene"},
   '>> ': {type: "hsynopsis"},
-  '/* ': {type: 'hnotes'},
+  '%% ': {type: 'hnotes'},
   '=> ': {type: "bookmark"},
   '!! ': {type: 'missing'},
   '// ': {type: 'comment'},
@@ -136,6 +136,5 @@ export const MARKUP = {
   //'))':
   //'==':
   //'??':
-  //'%%':
   //'::':
 }
