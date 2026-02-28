0b22f5bbce69f927aaba12e377538b09711dbf2d
Markus Koskimies <mkoskim@gmail.com>
2026-02-28 16:44:57 +0200

    Menu icon added

--- src/gui/common/icons.jsx
+++ src/gui/common/icons.jsx
@@ -68,6 +68,13 @@ const StarOutlined = createSvgIcon({
   </>
 })
 
+const Menu = createSvgIcon({
+  children: <>
+    <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+  </>
+})
+
+
 //-----------------------------------------------------------------------------
 // Arrows
 //-----------------------------------------------------------------------------
@@ -202,13 +209,13 @@ export const Icon = {
   Checked,
   Close,
   Search,
+  Menu,
   Quit: Close,
 
   Star: StarOutlined,
   Starred: Star,
   Circle: Placeholder, //Circle,
   Help: HelpOutline,
-  Menu: Placeholder, //Menu,
 
   ExpandMore: Placeholder, //ExpandMore,
 
