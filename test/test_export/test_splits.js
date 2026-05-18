ea6c420a1e73e81c8ee31de4b4b8e4adf254fcbe
Markus Koskimies <mkoskim@gmail.com>
2026-05-18 22:11:32 +0300

    Test export splitting

---
+++ test/test_export/test_splits.js
@@ -0,0 +1,7 @@
+import { installFakeIpc } from "../support/fakeIpc.js";
+
+installFakeIpc();
+
+console.log("Split export tests... (placeholder)");
+
+// TODO: tests for splitBatches(section, split)
