67aee4f345206b4f6f5c1c20e6cf4cfb2041f965
Markus Koskimies <mkoskim@gmail.com>
2026-07-28 13:19:07 +0300

    Package updates

--- src/util/compress.js
+++ src/util/compress.js
@@ -7,14 +7,14 @@
 //*****************************************************************************
 
 import isGzip from "is-gzip"
-import pako from "pako"
+import { deflate, inflate } from 'pako';
 
 export {isGzip}
 
 export function gzip(buffer, options) {
-  return pako.gzip(buffer, options);
+  return deflate(buffer, options);
 }
 
 export function gunzip(buffer, options) {
-  return pako.ungzip(buffer, options);
+  return inflate(buffer, options);
 }
